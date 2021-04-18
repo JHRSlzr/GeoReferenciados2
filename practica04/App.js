9a7714ec6608e52f3c1bda9e48d736555c561d7b
hernan reyes <hernanryz@hotmail.com>
2021-04-17 21:51:24 -0500

    practica04

--- practica04/App.js
+++ practica04/App.js
@@ -1,18 +1,22 @@
 function iniciaMapa(){
 
+
     var coordenadas = {  lat: 21.152639, lng:  -101.711598 };
 
     var propiedades1 = {
         center: coordenadas,
         zoom: 12
     };
+
     var mapa1 = new google.maps.Map(document.getElementById('mapa1'),propiedades1);
-    
+
+
     var propiedades2 = {
         center: coordenadas,
         zoom: 12,
         disableDefaultUI: true
     };
+
     var mapa2 = new google.maps.Map(document.getElementById('mapa2'),propiedades2);
 
     var propiedades3 = {
@@ -21,7 +25,9 @@ function iniciaMapa(){
         zoomControl: false,
         scaleControl: false
     };
+
     var mapa3 = new google.maps.Map(document.getElementById('mapa3'),propiedades3);
+
 
     var propiedades4 = {
         center: coordenadas,
