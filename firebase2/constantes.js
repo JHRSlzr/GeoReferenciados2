db79704d6cfdec81e5e78675d37d4102d5e01b86
hernan reyes <hernanryz@hotmail.com>
2021-05-03 23:05:37 -0500

    practicas firebase

---
+++ firebase2/constantes.js
@@ -0,0 +1,5 @@
+  
+    const productoslista = document.querySelector("#lista");
+    const formulario = document.querySelector("#forma");
+    const formularioeditar = document.querySelector("#formaeditar");
+    
